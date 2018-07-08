0377b2e27ba08789aa2b76e7b2be99eb2fd7bda6
Lakshmi Anand K <smtwkla@gmail.com>
2018-07-08 19:19:37 +0530

    json format handling

--- mqtt_conn.py
+++ mqtt_conn.py
@@ -4,4 +4,4 @@ import mqtt_config
 conf=mqtt_config.read_mqtt_conf()
 
 print "config:"
-print conf['mqtt_host'] + " " + conf['mqtt_port'] + " " +  conf['mqtt_username'] + " " + conf['mqtt_password']
+print conf['mqtt_host'] + " " + str(conf['mqtt_port']) + " " +  conf['mqtt_username'] + " " + conf['mqtt_password']
