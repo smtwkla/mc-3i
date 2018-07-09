adb86242e0aff53aecc7625abbdc7e71cc314a05
Lakshmi Anand K <smtwkla@gmail.com>
2018-07-09 05:45:00 +0530

    RuleList list created

--- mqtt_conn.py
+++ mqtt_conn.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import mqtt_config as mqtt_c
 import topic_config as topic_c
 import DBC as dbc
+import Rule as RuleClass
 
 #Load MQTT Server Config from JSON File
 conf=mqtt_c.read_mqtt_conf()
@@ -11,11 +12,13 @@ print "MQTT Server Config:"
 print conf['mqtt_host'] + ":" + str(conf['mqtt_port']) + " " +  conf['mqtt_username'] + " " + conf['mqtt_password']
 
 def on_connect(client, userdata, flags,rc):
-	print("Connected with result code "+str(rc))
-	client.subscribe("test")
+    print("Connected with result code "+str(rc))
+# Subscribe to all rule topics defined in RuleList
+    for aRule in RuleList:
+        client.subscribe(aRule.getTopic())
 
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg.payload))
 
 #Create Database Class
 db = dbc.DBConnector()
@@ -26,11 +29,15 @@ db.connect()
 #Load Topics List from JSON File
 topic=topic_c.read_topic_conf()
 
-#Parse Topics to be subscribed to
-for a in topic.items():
-    print "Topic: "
-    print a[1]['TOPIC']
+RuleList=[]
 
+#Parse Topics to be subscribed to from JSON File and add it to RuleList[]
+for aTopic in topic.items():
+    r=RuleClass.Rule(db,name=aTopic[0], topic=aTopic[1]['TOPIC'], tablename=aTopic[1]['TABLE'], insert=aTopic[1]['INSERT'])
+    RuleList.append(r)
+
+
+print str(len(RuleList)) + " topic rules added."
 
 #MQTT paho Client
 client = mqtt.Client()
