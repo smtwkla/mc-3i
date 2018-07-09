6b7e83c9cfd4a7be592c31e292bad473a7ac69e0
Lakshmi Anand K <smtwkla@gmail.com>
2018-07-09 06:48:09 +0530

    connect to db code

--- DBC.py
+++ DBC.py
@@ -1,4 +1,5 @@
 import json
+import  pymysql.cursors
 
 def read_db_conf():
     with open('db_config.json', 'r') as s:
@@ -11,10 +12,21 @@ class DBConnector(object):
     def __init__(self):
         super(DBConnector, self).__init__()
         c=read_db_conf()
-        print c['DB']['HOST']
-        print c['DB']['PORT']
+        self.db_host= c['DB']['HOST']
+        self.db_port= c['DB']['PORT']
+        self.db_user= c['DB']['USER']
+        self.db_passwd= c['DB']['PASS']
+        self.db_db= c['DB']['DB']
 
-    def connect(self):        
+    def connect(self):
+        print "Connecting to database " + self.db_host + ":" + str(self.db_port)
+        # Connect to the database
+        self.connection = pymysql.connect(host=self.db_host,
+                                     user=self.db_user,
+                                     password=self.db_passwd,
+                                     db=self.db_db,
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
         return True
 
     def insert(self):
