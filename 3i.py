05f3ee931e18b53d91269f5e5b4b500d96d97490
kla <smtwkla@gmail.com>
2018-08-08 16:04:07 +0530

    logging level, file name parameters added

--- 3i.py
+++ 3i.py
@@ -12,29 +12,41 @@ import logging
 # Create App Class
 Env3i = Env3iClass.Env3iClass()
 
-# Configure Logger
-FORMAT = '%(asctime)-15s : %(levelname)s : %(module)s : %(message)s'
-logging.basicConfig(format=FORMAT)
-
 # Set defaults
 Env3i.db_conf = "conf/"
 Env3i.conf_root = "conf/"
 
+l_filename = None
+l_level = None
 # Process command line arguments
 if len(sys.argv) >= 1:
     (switches, flags) = process_args(sys.argv)
     for i in switches:
         if i[0] == "-d":
-            Env3i.db_conf = i[1]
+            Env3i.db_conf = str(i[1])
         elif i[0] == "-c":
             Env3i.conf_root = i[1]
+        elif i[0] == "-L":
+            if i[1].strip().upper() == "DEBUG":
+                l_level = logging.DEBUG
+        elif i[0] == "-LF":
+            l_filename = i[1]
+        else:
+            logging.critical("Error : Unknown command line switch " + i[0])
+            exit(1)
     for i in flags:
         if "MODE_ACTION" == i.strip().upper():
             Env3i.action_daemon_mode = True
-            logging.info("Starting in Action Rule Mode...")
+        else:
+            logging.critical("Error : Unknown command line flag " + i)
+            exit(1)
 
-logging.info("DB Conf path:", Env3i.db_conf)
-logging.info("Conf Root path:", Env3i.conf_root)
+# Configure Logger
+FORMAT = '%(asctime)-15s : %(levelname)s : %(module)s : %(message)s'
+logging.basicConfig(format=FORMAT, filename=l_filename, level=l_level)
+
+logging.info("DB Conf path: " + Env3i.db_conf)
+logging.info("Conf Root path: " + Env3i.conf_root)
 
 # Create Database Class
 Env3i.dbc = dbc.DBConnector(Env3i.conf_root)
@@ -50,6 +62,9 @@ Env3i.topic_root = m_conf['topic_root']
 Env3i.rules = []
 
 if Env3i.action_daemon_mode:
+
+    logging.info("Starting in Action Rule Mode...")
+
     # Load Direct2DB Topics List from JSON File
     topic = read_c.read_direct2db_topic_conf(Env3i.conf_root)
 
